16ef472a8da6f2d7c6f623cbc1c06a9ac4b04f19
aashiru <aashiru@salesforce.com>
2018-08-16 10:24:38 -0700

    files converted to sfdx format

--- .sfdx/typings/lwc/apex/ProductListController.d.ts
+++ .sfdx/typings/lwc/apex/ProductListController.d.ts
@@ -1,48 +1,48 @@
-declare module "@apex/ProductListController.getReleatedListsMetadata" {
-  export function getReleatedListsMetadata(param: {objectId: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getReleatedListsMetadata" {
+  export default function getReleatedListsMetadata(param: {objectId: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getReleatedListMetadata" {
-  export function getReleatedListMetadata(param: {objectId: any, relatedListLabel: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getReleatedListMetadata" {
+  export default function getReleatedListMetadata(param: {objectId: any, relatedListLabel: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getOpportunityProductMetadata" {
-  export function getOpportunityProductMetadata(param: {objectId: any, relatedListLabel: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getOpportunityProductMetadata" {
+  export default function getOpportunityProductMetadata(param: {objectId: any, relatedListLabel: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getReleatedListItems" {
-  export function getReleatedListItems(param: {objectId: any, relatedlistName: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getReleatedListItems" {
+  export default function getReleatedListItems(param: {objectId: any, relatedlistName: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.saveRelatedListItems" {
-  export function saveRelatedListItems(param: {jsonData: any, initialData: any, selectedData: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.saveRelatedListItems" {
+  export default function saveRelatedListItems(param: {jsonData: any, initialData: any, selectedData: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getLookupCandidates" {
-  export function getLookupCandidates(param: {refObjName: any, searchTerm: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getLookupCandidates" {
+  export default function getLookupCandidates(param: {refObjName: any, searchTerm: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getObject" {
-  export function getObject(param: {objectId: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getObject" {
+  export default function getObject(param: {objectId: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.createRelatedObject" {
-  export function createRelatedObject(param: {objectId: any, objectName: any, jsonData: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.createRelatedObject" {
+  export default function createRelatedObject(param: {objectId: any, objectName: any, jsonData: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getBillingGroups" {
-  export function getBillingGroups(param: {objectId: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getBillingGroups" {
+  export default function getBillingGroups(param: {objectId: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getSelectedBillingGroups" {
-  export function getSelectedBillingGroups(param: {objectId: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getSelectedBillingGroups" {
+  export default function getSelectedBillingGroups(param: {objectId: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.createBillingGroup" {
-  export function createBillingGroup(param: {billingGroup: any, itemId: any}): Promise<any>;
+declare module "@salesforce/apex/ProductListController.createBillingGroup" {
+  export default function createBillingGroup(param: {billingGroup: any, itemId: any}): Promise<any>;
 }
-declare module "@apex/ProductListController.getDomesticPartnershipAllowed" {
-  export function getDomesticPartnershipAllowed(): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getDomesticPartnershipAllowed" {
+  export default function getDomesticPartnershipAllowed(): Promise<any>;
 }
-declare module "@apex/ProductListController.getRequestedRatingClass" {
-  export function getRequestedRatingClass(): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getRequestedRatingClass" {
+  export default function getRequestedRatingClass(): Promise<any>;
 }
-declare module "@apex/ProductListController.getRequestedCobraAdmin" {
-  export function getRequestedCobraAdmin(): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getRequestedCobraAdmin" {
+  export default function getRequestedCobraAdmin(): Promise<any>;
 }
-declare module "@apex/ProductListController.getRetroCancellationAllowance" {
-  export function getRetroCancellationAllowance(): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getRetroCancellationAllowance" {
+  export default function getRetroCancellationAllowance(): Promise<any>;
 }
-declare module "@apex/ProductListController.getRequestedCobraAdminOptions" {
-  export function getRequestedCobraAdminOptions(): Promise<any>;
+declare module "@salesforce/apex/ProductListController.getRequestedCobraAdminOptions" {
+  export default function getRequestedCobraAdminOptions(): Promise<any>;
 }
